583155ad62bcb5e9cd64af38a4c4925accc2e296
konrad <konradcz02@gmail.com>
2022-01-04 18:31:44 +0100

    Hello World

--- src/views/Root.js
+++ src/views/Root.js
@@ -3,12 +3,7 @@ import React from 'react';
 console.log('hello');
 
 const Root = () => {
-  return (
-    <>
-      <input type="text" />
-      <h1>siema world</h1>
-    </>
-  );
+  return <h1>Hello world</h1>;
 };
 
 export default Root;
