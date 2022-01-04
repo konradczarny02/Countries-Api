5e7e4496a1fead05a2f053b9dc25b7de810069a7
konrad <konradcz02@gmail.com>
2022-01-04 18:30:13 +0100

    initial setup

---
+++ src/views/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+console.log('hello');
+
+const Root = () => {
+  return (
+    <>
+      <input type="text" />
+      <h1>siema world</h1>
+    </>
+  );
+};
+
+export default Root;
